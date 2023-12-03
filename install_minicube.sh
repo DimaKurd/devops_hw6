d67885129a6f1f1f120bc615e07d5a1ac838ef6b
DimaKurd <43163420+DimaKurd@users.noreply.github.com>
2023-12-04 02:14:11 +0300

    tools install scripts

---
+++ install_minicube.sh
@@ -0,0 +1,8 @@
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
+sudo usermod -aG docker $USER && newgrp docker
+
+minikube start
+minikube kubectl -- get po -A
+alias kubectl="minikube kubectl --"
